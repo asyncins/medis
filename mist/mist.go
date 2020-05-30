3a23c522606c4435d305824ffea6c4766525b7aa
AsyncIns <47288304+asyncins@users.noreply.github.com>
2020-05-31 01:11:07 +0800

    改用大数随机函数，性能提升 25 倍，同时删减部分相关代码

--- mist/mist.go
+++ mist/mist.go
@@ -19,30 +19,21 @@
 package mist
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
-	"medis/common"
-	"time"
+	"math/big"
 )
 
-const saltBit = uint(8)                       // 随机因子二进制位数
-const sequenceBit = uint(8)                   // 序列号二进制位数
-const saltMax = int64(1<<saltBit - 1)         // 随机因子数值上限
-const sequenceMax = int64(1<<sequenceBit - 1) // 序列号数值上限
-const saltShift = sequenceBit                 // 随机因子移位数
-const increasShift = saltBit + sequenceBit    // 高位数移位数
-
-/* 生成区间范围内的随机数 */
-func RandInt64(duration int64) int64 {
-	rand.Seed(duration) // 时间戳作为随机因子
-	return rand.Int63n(common.RandMax)
-}
+const saltBit = uint(8)                  // 随机因子二进制位数
+const saltShift = uint(8)                // 随机因子移位数
+const increasShift = saltBit + saltShift // 高位数移位数
 
 /* 生成唯一编号 */
 func Generate(increas int64) int64 {
-	now := time.Now()
-	saltA := RandInt64(now.UnixNano() / 1e1)
-	saltB := RandInt64(now.UnixNano() / 1e2)
+	randA, _ := rand.Int(rand.Reader, big.NewInt(255))
+	saltA := randA.Int64()
+	randB, _ := rand.Int(rand.Reader, big.NewInt(255))
+	saltB := randB.Int64()
 	fmt.Println(saltA, saltB)
 	mist := int64((increas << increasShift) | (saltA << saltShift) | saltB) // 通过位运算实现自动占位
 	return mist
